67d83964cf0c937d972c1211f89380ac1406ef05
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-07-01 17:18:22 -0500

    modified state structure to add fg and rename bg

--- src/App.js
+++ src/App.js
@@ -15,17 +15,23 @@ class App extends Component {
     if (!stored) {
       return null;
     } else {
-      let { background, syntax } = JSON.parse(stored);
+      let { bg, fg, syntax } = JSON.parse(stored);
       return {
-        background: background || '#fff',
+        bg: bg || '#ffffff',
+        fg: fg || '#000000',
         syntax: syntax || ['#fff']
       }
     }
   };
 
   updateBG = (c, e) => {
-    let background = c.hex;
-    this.setState({ background });
+    let bg = c.hex;
+    this.setState({ bg });
+  };
+
+  updateFG = (c, e) => {
+    let fg = c.hex;
+    this.setState({ fg });
   };
 
   updateSyntax = (syntax) => {
@@ -45,11 +51,23 @@ class App extends Component {
             <h3>Background</h3>
             <div className='picker'>
               <ChromePicker
-                color={this.state.background}
+                color={this.state.bg}
                 onChange={this.updateBG}
               />
               <div className='palette'>
-                <Swatch color={this.state.background} active={true} />
+                <Swatch color={this.state.bg} active={true} />
+              </div>
+            </div>
+          </section>
+          <section>
+            <h3>Foreground</h3>
+            <div className='picker'>
+              <ChromePicker
+                color={this.state.fg}
+                onChange={this.updateFG}
+              />
+              <div className='palette'>
+                <Swatch color={this.state.fg} active={true} />
               </div>
             </div>
           </section>
@@ -60,7 +78,7 @@ class App extends Component {
         </main>
         <header>
           <h1 className="title">Color Picker</h1>
-          <textarea defaultValue={JSON.stringify(this.state)} rows='5'></textarea>
+          <textarea value={JSON.stringify(this.state)} rows='5'></textarea>
           <button onClick={this.save} type='Submit'>
             Save
           </button>
