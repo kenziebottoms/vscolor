b2d11f3b8adf617e994fb7ec57d0edc57968ae0d
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-06-06 11:39:29 -0500

    move build folder to root

---
+++ precache-manifest.8fb9853926657a9f583303120d74a35a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6bfcdd8bbb44a2845e4cf988f69e01",
+    "url": "/vscolor/index.html"
+  },
+  {
+    "revision": "778b365fa12e1ff333c3",
+    "url": "/vscolor/static/css/main.04cd7f11.chunk.css"
+  },
+  {
+    "revision": "e001ea0d0ee4f0880fa7",
+    "url": "/vscolor/static/js/2.e3a51270.chunk.js"
+  },
+  {
+    "revision": "778b365fa12e1ff333c3",
+    "url": "/vscolor/static/js/main.4d370d13.chunk.js"
+  },
+  {
+    "revision": "264a20d103fe66c668dc",
+    "url": "/vscolor/static/js/runtime~main.071f9e06.js"
+  }
+]);
