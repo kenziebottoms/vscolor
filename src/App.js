8e75c75d6d28f1b2fa7f3bfc4d6072e58b5db97d
Kenzie Bottoms <kenziebottoms@gmail.com>
2023-09-01 17:14:54 -0500

    functionify app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ChromePicker } from 'react-color';
 import {
   faPaste,
@@ -17,192 +17,193 @@ import { genTheme, genSettings, gradient } from './Theme';
 
 import { loadLocalSpine, saveLocalSpine, validateSpine } from './Spine';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { theme: null, active: 'bg' };
-    this.state.theme = loadLocalSpine();
-  }
+const App = () => {
+  const [theme, setTheme] = useState(loadLocalSpine());
+  const [activeSwatch, setActiveSwatch] = useState('bg');
 
-  importFromClipboard = () =>
+  const importFromClipboard = () =>
     navigator.clipboard
       .readText()
-      .then(clipboard => this.importSpine(clipboard))
+      .then(clipboard => importSpine(clipboard))
       .catch(e => console.log(e));
 
   // get color value of active swatch
-  getActiveColor = () =>
-    this.state.active.indexOf('syntax') !== -1
-      ? this.state.theme.syntax[this.state.active.split(':')[1]]
-      : this.state.theme[this.state.active];
+  const getActiveColor = () =>
+    activeSwatch.indexOf('syntax') !== -1
+      ? theme.syntax[activeSwatch.split(':')[1]]
+      : theme[activeSwatch];
 
   // update color value of active swatch
-  updateActiveColor = (c, _e) => {
-    // copy theme to update
-    //   like a POST intead of a PUT
-    let stateUpdate = { theme: this.state.theme };
-    // syntax colors
-    if (this.state.active.indexOf('syntax') !== -1) {
-      stateUpdate.theme.syntax[this.state.active.split(':')[1]] = c.hex;
+  const updateActiveColor = (c, _e) => {
+    if (activeSwatch.includes('syntax')) {
+      setTheme({
+        ...theme,
+        syntax: {
+          ...theme.syntax,
+          [activeSwatch.split(':')[1]]: c.hex
+        }
+      });
+    } else {
+      setTheme({
+        ...theme,
+        [activeSwatch]: c.hex
+      });
     }
-    // non-syntax
-    else {
-      stateUpdate.theme[this.state.active] = c.hex;
-    }
-    this.setState(stateUpdate);
   };
 
   // handle a click on swatch `i`
-  clickSwatch = i => {
-    if (this.state.active === 'syntax:' + i) {
-      let syntaxUpdate = this.state.theme.syntax.slice();
+  const clickSwatch = i => {
+    if (activeSwatch === 'syntax:' + i) {
+      let syntaxUpdate = theme.syntax.slice();
       syntaxUpdate.splice(i, 1);
-      this.updateSyntax(syntaxUpdate);
+      updateSyntax(syntaxUpdate);
     } else {
-      this.setState({ active: 'syntax:' + i });
+      setActiveSwatch(`syntax:${i}`);
     }
   };
 
-  copyToClipboard = slug => navigator.clipboard.writeText(slug);
+  const copyToClipboard = slug => navigator.clipboard.writeText(slug);
 
   // add new blank swatch
-  addSwatch = () => {
-    let colors = this.state.theme.syntax.slice();
+  const addSwatch = () => {
+    let colors = theme.syntax.slice();
     let active = colors.push('#fff') - 1; // push returns the new length of the array
-    this.updateSyntax(colors);
-    this.setState({ active: 'syntax:' + active });
+    updateSyntax(colors);
+    setActiveSwatch(`syntax:${active}`);
   };
 
-  // update this.state.theme.syntax without overwriting anything else
-  updateSyntax = syntax => {
-    let stateUpdate = { theme: this.state.theme };
-    stateUpdate.theme.syntax = syntax;
-    this.setState({ stateUpdate });
+  // update theme.syntax without overwriting anything else
+  const updateSyntax = syntax => {
+    setTheme({
+      ...theme,
+      syntax,
+    })
   };
 
-  shuffleSyntax = () => this.updateSyntax(shuffle(this.state.theme.syntax));
+  const shuffleSyntax = () => setTheme({
+    ...theme,
+    syntax: shuffle(theme.syntax)
+  });
 
   // validate pasted spine and use it if valid
-  importSpine = spine => {
+  const importSpine = spine => {
     let error = validateSpine(spine);
     if (error) {
       console.error(error.message);
     } else {
-      this.setState({ theme: JSON.parse(spine) });
+      setTheme(JSON.parse(spine));
     }
   };
 
-  render() {
-    return (
-      <div
-        className="wrapper"
+  return (
+    <div
+      className="wrapper"
+      style={{
+        background: theme.bg,
+        color: theme.fg,
+      }}
+    >
+      <section
+        className="controls"
         style={{
-          background: this.state.theme.bg,
-          color: this.state.theme.fg,
+          background: theme.bg,
+          color: theme.ui + '55',
         }}
       >
-        <section
-          className="controls"
-          style={{
-            background: this.state.theme.bg,
-            color: this.state.theme.ui + '55',
-          }}
-        >
-          <Control
-            onClick={this.importFromClipboard}
-            icon={faPaste}
-            label='Import from clipboard'
-            {...this.state.theme}
-          />
-          
-          <Control
-            onClick={() => saveLocalSpine(this.state.theme)}
-            icon={faSave}
-            label='Save theme'
-            {...this.state.theme}
-          />
+        <Control
+          onClick={importFromClipboard}
+          icon={faPaste}
+          label='Import from clipboard'
+          {...theme}
+        />
+        
+        <Control
+          onClick={() => saveLocalSpine(theme)}
+          icon={faSave}
+          label='Save theme'
+          {...theme}
+        />
 
-          <Control
-            onClick={() => this.copyToClipboard(JSON.stringify(this.state.theme))}
-            icon={faClipboardList}
-            label='Copy theme spine'
-            {...this.state.theme}
-          />
+        <Control
+          onClick={() => copyToClipboard(JSON.stringify(theme))}
+          icon={faClipboardList}
+          label='Copy theme spine'
+          {...theme}
+        />
 
-          <Control
-            onClick={() => this.copyToClipboard(JSON.stringify(genTheme(this.state.theme)))}
-            icon={faCode}
-            label='Copy theme code'
-            {...this.state.theme}
-          />
+        <Control
+          onClick={() => copyToClipboard(JSON.stringify(genTheme(theme)))}
+          icon={faCode}
+          label='Copy theme code'
+          {...theme}
+        />
 
-          <Control
-            onClick={() => this.copyToClipboard(JSON.stringify(genSettings(this.state.theme)))}
-            icon={faCogs}
-            label='Copy workspace settings'
-            {...this.state.theme}
-          />
-        </section>
-        <section
-          className="sidebar"
-          style={{
-            background: gradient(
-              this.state.theme.bg,
-              gradient(this.state.theme.bg, this.state.theme.fg)['7.5']
-            )['50'],
-            color: this.state.theme.fg,
-          }}
-        >
-          <ChromePicker
-            color={this.getActiveColor()}
-            onChange={this.updateActiveColor}
-          />
-        </section>
+        <Control
+          onClick={() => copyToClipboard(JSON.stringify(genSettings(theme)))}
+          icon={faCogs}
+          label='Copy workspace settings'
+          {...theme}
+        />
+      </section>
+      <section
+        className="sidebar"
+        style={{
+          background: gradient(
+            theme.bg,
+            gradient(theme.bg, theme.fg)['7.5']
+          )['50'],
+          color: theme.fg,
+        }}
+      >
+        <ChromePicker
+          color={getActiveColor()}
+          onChange={updateActiveColor}
+        />
+      </section>
 
-        <section className="content">
-          <div>
-            {[
-              ['Background', 'bg'],
-              ['Foreground', 'fg'],
-              ['UI accent', 'ui'],
-              ['Positive color', 'pos'],
-              ['Negative color', 'neg'],
-            ].map(([label, slug]) => (
-              <>
-                <Swatch
-                  onClick={() => this.setState({ active: slug })}
-                  color={this.state.theme[slug]}
-                  active={this.state.active === slug}
-                />
-                <h3>{label}</h3>
-              </>
-            ))}
-            
-            <div className="syntax">
-              {this.state.theme.syntax.map((color, i) => (
-                <Swatch
-                  key={i}
-                  color={color}
-                  onClick={() => this.clickSwatch(i)}
-                  active={this.state.active === `syntax:${i}`}
-                />
-              ))}
-
+      <section className="content">
+        <div>
+          {[
+            ['Background', 'bg'],
+            ['Foreground', 'fg'],
+            ['UI accent', 'ui'],
+            ['Positive color', 'pos'],
+            ['Negative color', 'neg'],
+          ].map(([label, slug]) => (
+            <>
               <Swatch
-                key={'new'}
-                isNew
-                onClick={this.addSwatch}
+                onClick={() => setActiveSwatch(slug)}
+                color={theme[slug]}
+                active={activeSwatch === slug}
               />
-            </div>
-            <h3>
-              Syntax colors&nbsp;
-              <button onClick={this.shuffleSyntax}>Shuffle</button>
-            </h3>
+              <h3>{label}</h3>
+            </>
+          ))}
+          
+          <div className="syntax">
+            {theme.syntax.map((color, i) => (
+              <Swatch
+                key={i}
+                color={color}
+                onClick={() => clickSwatch(i)}
+                active={activeSwatch === `syntax:${i}`}
+              />
+            ))}
+
+            <Swatch
+              key={'new'}
+              isNew
+              onClick={addSwatch}
+            />
           </div>
-        </section>
-      </div>
-    );
-  }
+          <h3>
+            Syntax colors&nbsp;
+            <button onClick={shuffleSyntax}>Shuffle</button>
+          </h3>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
